dd2e02c596a41f9c1638b58c7c92751d7ae08966
JackyFWong <jwong2398@gmail.com>
2019-05-03 02:08:19 -0400

    fixed cmake error, typo in test, tested with camera

--- test/raspitest/simpletest_raspicam_cv.cpp
+++ test/raspitest/simpletest_raspicam_cv.cpp
@@ -2,41 +2,39 @@
 #include <iostream>
 #include <raspicam/raspicam_cv.h>
 
-int main ( int argc, char** argv ) {
+using namespace std;
 
-    time_t timer_begin, timer_end;
-    raspicam::RaspiCam_Cv Camera;
-    cv:: Mat image;
-    int nCount = 100;
+int main (int argc, char **argv) {
+	time_t timer_begin, timer_end;
+	raspicam::RaspiCam_Cv Camera;
+	cv::Mat image;
+	int nCount=100;
 
-    // camera param
-    Camera.set( CV_CAP_PROP_FORMAT, CV_BUG1 );
-    // open
-    cout << "Opening camera..." << endl;
-    if ( !Camera.open() ) {
-        cerr << "Error opening camera." << endl;
-        return -1;
-    }
+	Camera.set(cv::CAP_PROP_FORMAT, CV_8UC1);
 
-    // capture
-    cout << "Capturing " << nCount << " frames ..." << endl;
-    time ( &timer_begin);
-    for (int i=0; i < nCount; i++) {
-        Camera.grab();
-        Camera.retrieve( image );
-        if (i % 5 == 0) cout << "\r captured " << i << " images" << std::flush;
-    }
-    cout << "Stop camera." << endl;
-    Camera.release();
+	cout << "Opening camera..." << endl;
+	if (!Camera.open()) {
+		cerr << "Error opening the camera" << endl;
+		return -1;
+	}
 
-    // time stats
-    time ( &timer_end);
-    double secElapased = difftime( timer_end, timer_begin );
-    cout << secElapsed << " seconds for " << nCount << " frames : FPS = " << 
-        (float)((float)nCount / secElapased) << endl;
+	cout << "Capturing " << nCount << " frames ...." << endl;
+	time (&timer_begin);
+	for (int i=0; i< nCount; i++) {
+		Camera.grab();
+		Camera.retrieve(image);
+		if (i%5==0) cout << "\r captured " << i << " images" 
+			<< std::flush;
+	}
 
-    // save img
-    cv::imwrite("raspicam_cv_image.jpg", image);
-    cout << "Image saved at raspicam_cv_image.jpg" << endl;
+	cout << "Stop camera..." << endl;
+	Camera.release();
 
+	time (&timer_end);
+	double secondsElasped = difftime(timer_end, timer_begin);
+	cout << secondsElasped << " seconds for " << nCount << 
+		" frames : FPS = " << (float)((float)(nCount)/secondsElasped)
+		<< endl;
+	cv::imwrite("raspicam_cv_image.jpg", image);
+	cout << "Image saved at raspicam_cv_image.jpg" << endl;
 }
